64c81afe0eaa2b8935695879bda098a58733559c
Gregor Baird <gpbaird@c28-0112-21.ad.mtu.edu>
2024-10-08 13:18:21 -0400

    gregor

---
+++ Bicycle.java
@@ -0,0 +1,9 @@
+class Bicycle extends Vehicle  {
+    //uses same logic as car but methods result in different outputs for bycicle
+    void start() {
+        System.out.println("Bicycle starts");
+    }
+    void honk() {
+        System.out.println("Bell rings");
+    }
+}
--- Road.java
+++ Road.java
@@ -1,7 +1,10 @@
 public class Road {
     public static void main(String[] args) {
-        Vehicle v = new Car();
+        Vehicle v  = new Car();
+        Vehicle b = new Bicycle();
         v.start();
         v.honk();
+        b.start();
+        b.honk();
     }
 }
